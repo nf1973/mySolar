dd67976a88f6bea346baa4dd9083c566aac2c22b
nf1973 <neil.fensom@gmail.com>
2023-01-01 17:21:43 +0100

    Switch "getavailablemonths" result to be an Array

--- backend/contollers/Controllers.js
+++ backend/contollers/Controllers.js
@@ -32,7 +32,8 @@ export const getSolarLogsByDate = async (req, res) => {
 export const getAvailableMonths = async (req, res) => {
   try {
     const response = await SolarLogs.sequelize.query(
-      "SELECT DISTINCT SUBSTRING(date,1,6) FROM solarlogs;"
+      "SELECT DISTINCT SUBSTRING(date,1,6) FROM solarlogs;",
+      { type: SolarLogs.sequelize.QueryTypes.SELECT }
     );
     res.status(200).json(response);
   } catch (error) {
